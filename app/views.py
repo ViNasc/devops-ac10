8f3edd3080bc8153a08a53517c6cb4fa11f87671
ViNasc <43283513+ViNasc@users.noreply.github.com>
2018-11-12 08:55:20 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -61,7 +61,7 @@ def cadastro_cursos(request):
             'cursos': Curso.objects.all(),
             'year':datetime.now().year,
         })
-    
+    )
             
 def cadastro_vestibulares(request): 
     assert isinstance(request, HttpRequest)
