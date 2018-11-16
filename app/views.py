9d8048c6682ded0533b75d5e4bb140494643ca0c
ViNasc <43283513+ViNasc@users.noreply.github.com>
2018-11-16 13:27:51 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -72,7 +72,7 @@ def cadastro_vestibulares(request):
         'app/cadastro_vestibulares.html', 
         context_instance = RequestContext(request,
         {
-            'title':'Cadastro de vestibulares’,
+            'title':'Cadastro de vestibulares',
             'vestibulares': Vestibular.objects.all( ),
             'year':datetime.now().year,
         })
